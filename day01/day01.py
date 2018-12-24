fa2fb5740b3f6f4974dfb6ca1a0b72523f02ea25
Jan Christoph Terasa <christoph@kohlio.de>
2018-12-24 10:38:29 +0100

    Day 1.

---
+++ day01/day01.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import fileinput
+
+def freqs(lines):
+    freq = 0
+    for line in lines:
+        op = line[0]
+        num = int(line[1:])
+        if op == "+":
+            freq += num
+        else:
+            freq -= num
+        yield freq
+
+def part1(lines):
+    f = 0
+    for fr in freqs(lines):
+        f = fr
+    return f
+
+def part2(lines):
+    import itertools
+
+    s = set()
+    l = itertools.cycle(lines)
+    for fr in freqs(l):
+        if fr not in s:
+            s.add(fr)
+        else:
+            return fr
+
+if __name__ == '__main__':
+    lines = [x for x in fileinput.input()]
+    print("Part 1:", part1(lines))
+    print("Part 2:", part2(lines))
