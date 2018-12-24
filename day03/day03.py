1ddcf2c35439e83c01f875b1e79e81e554a92ab1
Jan Christoph Terasa <christoph@kohlio.de>
2018-12-24 11:21:31 +0100

    Day 3. Pt. 1.

---
+++ day03/day03.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import fileinput
+import re
+
+r = re.compile(r"#\d+ @ (\d+),(\d+): (\d+)x(\d+)")
+
+def parse(line):
+    m = r.match(line)
+    return [int(x) for x in m.groups()]
+
+def part1(lines):
+    fabric = [[0 for i in range(2000)] for j in range(2000)]
+
+    for line in lines:
+        l, t, w, h = parse(line)
+        for i in range(l, l+w):
+            for j in range(t, t+h):
+                fabric[i][j] += 1
+
+    s = 0
+    for row in fabric:
+        for item in row:
+            if item > 1:
+                s += 1
+    return s
+
+
+def part2(lines):
+    pass
+
+if __name__ == '__main__':
+    lines = [x.strip() for x in fileinput.input()]
+    print("Part 1:", part1(lines))
+    print("Part 2:", part2(lines))
