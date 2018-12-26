7163dc5d249a846fc59d312f6aac4db10098f49b
Jan Christoph Terasa <christoph@kohlio.de>
2018-12-26 11:43:17 +0100

    Day 4, pt. 1.

---
+++ day04/day04.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import fileinput
+import re
+
+r = re.compile(r"\[(.*)] (.*)")
+
+def parse(line):
+    m = r.match(line)
+    return m.groups()
+
+def part1(lines):
+    import datetime
+
+    cron = []
+    for line in lines:
+        t, a = parse(line)
+        t = datetime.datetime.strptime(t, "%Y-%m-%d %H:%M")
+        if a.startswith("falls"):
+            a = -1
+        elif a.startswith("wakes"):
+            a = -2
+        else:
+            a = int(a.split()[1][1:])
+        cron.append((t, a))
+
+    cron = sorted(cron, key=lambda x: x[0])
+    
+    active = None
+    guards = {}
+    s = 0
+    e = 0
+    for t, a in cron:
+        if a >= 0:
+            if s:
+                e = t.minute
+                for i in range(s, e):
+                    guards[active][i] += 1
+            active = a
+            if a not in guards:
+                guards.update({a: [0 for i in range(60)]})
+        if a == -1:
+            s = t.minute
+            e = None
+        if a == -2:
+            e = t.minute
+            for i in range(s, e):
+                guards[active][i] += 1
+            s = None
+    
+    m = 0
+    mi = -1
+    mm = -1
+    for k, v in guards.items():
+        sv = sum(v)
+        if sv > m:
+            mi = k
+            m = sv
+            mm = max(zip(v, range(60)))[-1]
+
+    checksum = mi * mm
+    return checksum
+
+def part2(lines):
+    pass
+
+if __name__ == '__main__':
+    lines = [x.strip() for x in fileinput.input()]
+    print("Part 1:", part1(lines))
+    print("Part 2:", part2(lines))
